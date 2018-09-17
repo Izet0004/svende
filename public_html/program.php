e86957db303318d419ad89d571345d5b343d2ed1
Izæt <izet0004@elev.techcollege.dk>
2018-09-17 19:02:58 +0200

    backp

---
+++ public_html/program.php
@@ -0,0 +1,61 @@
+<?php
+require("assets/incl/header.php");
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="text-center p-2">PROGRAM</h1>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-lg-10 program">
+            <ul>
+                <li><a href="#" class="a-remove">ONSDAG</a></li>
+                <li><a href="#" class="a-remove">TORSDAG</a></li>
+                <li><a href="#" class="a-remove">FREDAG</a></li>
+                <li><a href="#" class="a-remove">LØRDAG</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-lg-10 program-info">
+            <p class="bg-red white program-info-heading">RØD SCENE</p>
+            <table class="table">
+                <tbody class="border-top-bot-red">
+                    <tr>
+                        <th class="pad-sides vert-middle font-32" scope="row">17:00</th>
+                        <td class="full-width font-36">PETER SOMMMER <span class="font-14 block">Rød Scene</span></td>
+                    </tr>
+                    <tr>
+                        <th class="pad-sides vert-middle font-32" scope="row">17:00</th>
+                        <td class="full-width font-36">PETER SOMMMER <span class="font-14 block">Rød Scene</span></td>
+                    </tr>
+                    <tr>
+                        <th class="pad-sides vert-middle font-32" scope="row">17:00</th>
+                        <td class="full-width font-36">PETER SOMMMER <span class="font-14 block">Rød Scene</span></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="col-lg-10 program-info">
+            <p class="bg-green white program-info-heading">GRØN SCENE</p>
+            <table class="table">
+                <tbody class="border-top-bot-green">
+                    <tr>
+                        <th class="pad-sides vert-middle font-32" scope="row">17:00</th>
+                        <td class="full-width font-36">PETER SOMMMER <span class="font-14 block">Rød Scene</span></td>
+                    </tr>
+                    <tr>
+                        <th class="pad-sides vert-middle font-32" scope="row">17:00</th>
+                        <td class="full-width font-36">PETER SOMMMER <span class="font-14 block">Rød Scene</span></td>
+                    </tr>
+                    <tr>
+                        <th class="pad-sides vert-middle font-32" scope="row">17:00</th>
+                        <td class="full-width font-36">PETER SOMMMER <span class="font-14 block">Rød Scene</span></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php require("assets/incl/footer.php")?>
